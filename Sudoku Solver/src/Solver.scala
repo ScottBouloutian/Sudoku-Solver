5949d2080c988db7338560823af6131caa4b36ea
Scott Bouloutian <ScottBouloutian@gmail.com>
2014-02-27 11:50:55 -0500

    Got the solver working, added inter-node simplifier
    Added DFS solver and functional checking for possible valid moves. Added
    a puzzle simplifier to solve the puzzle as much as possible between DFS
    nodes.

--- Sudoku Solver/src/Solver.scala
+++ Sudoku Solver/src/Solver.scala
@@ -1,5 +1,43 @@
+// Scott Bouloutian
+
 object Solver {
+  val symbols = List('.', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G')
+
   def main(args: Array[String]) {
     println("Sudoku Solver Initialized")
+    val list = new Array[Int](256)
+    var i = 0;
+    for (line <- io.Source.fromFile("res/sudoku.txt").getLines()) {
+      for (col <- (0 to 15)) {
+        list(i) = symbols.indexOf(line.charAt(col))
+        i = i + 1
+      }
+    }
+
+    val puzzle = new Sudoku(list)
+    DFS(puzzle)
+    println("Done Program")
   }
+
+  def simplify(puzzle: Sudoku)=puzzle.elements.indices.foreach(puzzle.solveSpace(_))
+
+  def DFS(puzzle: Sudoku): Boolean = {
+    if (puzzle.solved) {
+      println("Solution to the puzzle was found!")
+      println(puzzle)
+      true
+    } else {
+      var emptySpace = puzzle.nextOpenSpace
+      for (n <- 1 to 16) {
+        if (puzzle.validMoves(emptySpace).contains(n)) {
+          var newElements = puzzle.elements.updated(emptySpace, n)
+          var newPuzzle = new Sudoku(newElements)
+          simplify(newPuzzle)
+          DFS(newPuzzle)
+        }
+      }
+    }
+    false
+  }
+
 }
---
+++ Sudoku Solver/src/Sudoku.scala
@@ -0,0 +1,43 @@
+class Sudoku(val puzzleArray: Array[Int]) {
+  val elements = puzzleArray
+  val symbols = List('.', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G')
+  val nums=List(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+  
+  def index(row: Int, col: Int) = row * 16 + col
+  def at(row: Int, col: Int): Int = elements(index(row, col))
+  def solved:Boolean=(!elements.contains(0))
+  def getRow(i:Int)=i/16
+  def getCol(i:Int)=i%16
+  def getCell(i:Int)=((i/16)/4)*4+((i%16)/4)
+  def nextOpenSpace=elements.indexOf(0)
+  
+  def validMoves(i:Int): List[Int] = {
+    val element = elements(i)
+    val indexedList = elements.zipWithIndex;
+    val rowElements = indexedList.filter(e => getRow(e._2)==getRow(i) && e._2 != i).unzip._1
+    val colElements = indexedList.filter(e => getCol(e._2)==getCol(i) && e._2 != i).unzip._1
+    val cellElements = indexedList.filter(e => getCell(e._2)==getCell(i) && e._2 != i).unzip._1
+    val scopeElements = (rowElements ++ colElements ++ cellElements).distinct
+    nums.diff(scopeElements)
+  }
+  
+  def solveSpace(i:Int)={
+    if(elements(i)==0){
+      val moves=validMoves(i)
+      if(moves.size==1){
+        elements(i)=moves.head
+      }
+    }
+  }
+  
+  override def toString = {
+    var result = ""
+    for (row <- (0 to 15)) {
+      for (col <- (0 to 15)) {
+        result += symbols(at(row, col))
+      }
+      result += '\n'
+    }
+    result
+  }
+}
