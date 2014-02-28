705c916dddedc2237aedd782aa291b32a70f7276
Scott Bouloutian <ScottBouloutian@gmail.com>
2014-02-28 13:22:00 -0500

    Added concurrency

--- Sudoku Solver/src/Solver.scala
+++ Sudoku Solver/src/Solver.scala
@@ -1,24 +1,46 @@
-// Scott Bouloutian
+/**
+ * ************************
+ * Scott Bouloutian
+ * 2/27/2014
+ * Solver.scala
+ * Represents a Solver for
+ * 	a Sudoku Puzzle
+ *
+ * 	This solver is designed to read a 16x16 sudoku puzzle from a file
+ * and output its solution to the user. The file should use the numbers
+ * 1-9, the letters A-G, and the '.' symbol to represent an empty space.
+ * This program is a demonstration of the Scala programming language,
+ * utilizing both its object oriented and functional paradigms.
+ * ************************
+ */
+
+import scala.actors._
+import Actor._
+import scala.io._
 
 object Solver {
+  // The symbols used to represent the sudoku
   val symbols = List('.', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G')
+  // Solves the sudoku as much as possible without any guesswork by looking at constraints
   def simplify(puzzle: Sudoku) = puzzle.elements.indices.foreach(puzzle.solveSpace(_))
 
+  // Starting point of the program
   def main(args: Array[String]) {
     println("Sudoku Solver Initialized")
-    val puzzle = readFile("res/sudoku.txt")
-    val result=DFS(puzzle)
-    if(result._2==true){
+    val puzzle = readFile(args(0))
+    val result = DFS(puzzle)
+    if (result._1) {
       println("Solution Found")
-      println(result._1);
+      println(result._2);
     }
     println("Done Program")
   }
 
+  // Reads a 16x16 sudoku puzzle from a file
   def readFile(fileName: String): Sudoku = {
     val array = new Array[Int](256)
     var i = 0
-    for (line <- io.Source.fromFile(fileName).getLines()) {
+    for (line <- Source.fromFile(fileName).getLines()) {
       for (col <- (0 to 15)) {
         array(i) = symbols.indexOf(line.charAt(col))
         i = i + 1
@@ -27,20 +49,33 @@ object Solver {
     new Sudoku(array)
   }
 
-  def DFS(puzzle: Sudoku): (Sudoku, Boolean) = {
+  // The depth first search algorithm used to solve the sudoku
+  def DFS(puzzle: Sudoku): (Boolean, Sudoku) = {
     if (puzzle.solved) {
       println("solved")
-      (puzzle, true)
-    } else {
-      val emptySpace = puzzle.nextOpenSpace
-      val moves = puzzle.validMoves(emptySpace)
-      moves.foreach(e => branchDFS(new Sudoku(puzzle.elements.updated(emptySpace, e))))
+      return (true, puzzle)
     }
-    (puzzle, false)
+    val emptySpace = puzzle.nextOpenSpace
+    val moves = puzzle.validMoves(emptySpace)
+    // For each possible move in the empty space, guess that move and recurse concurrently
+    val caller = self
+    for (e <- moves) {
+      actor {
+        val newPuzzle = new Sudoku(puzzle.elements.updated(emptySpace, e))
+        simplify(newPuzzle)
+        caller ! DFS(newPuzzle)
+      }
+    }
+    // Receive the results of the concurrent DFS
+    for (i <- 1 to moves.size) {
+      receive {
+        case r:(Boolean,Sudoku) =>
+          if(r._1){
+            return (true,r._2)
+          }
+      }
+    }
+    return (false, puzzle)
   }
 
-  def branchDFS(puzzle:Sudoku) = {
-    simplify(puzzle)
-    DFS(puzzle)
-  }
 }
--- Sudoku Solver/src/Sudoku.scala
+++ Sudoku Solver/src/Sudoku.scala
@@ -1,62 +1,64 @@
-/**************************
+/**
+ * ************************
  * Scott Bouloutian
  * 2/27/2014
  * Sudoku.scala
  * Represents a Sudoku puzzle
- **************************/
+ * ************************
+ */
 
 class Sudoku(val puzzleArray: Array[Int]) {
-  
-  
-  val elements = puzzleArray
+  val elements = puzzleArray // The array containing the sudoku elements
   val symbols = List('.', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G')
-  val nums=List(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
-  
-  def index(row: Int, col: Int) = row * 16 + col
-  def at(row: Int, col: Int): Int = elements(index(row, col))
-  def solved:Boolean=(!elements.contains(0))
-  def getRow(i:Int)=i/16
-  def getCol(i:Int)=i%16
-  def getCell(i:Int)=((i/16)/4)*4+((i%16)/4)
-  def nextOpenSpace=elements.indexOf(0)
-  
-  def validMoves(i:Int): List[Int] = {
-    val element = elements(i)
-    val indexedList = elements.zipWithIndex;
-    val rowElements = indexedList.filter(e => getRow(e._2)==getRow(i) && e._2 != i).unzip._1
-    val colElements = indexedList.filter(e => getCol(e._2)==getCol(i) && e._2 != i).unzip._1
-    val cellElements = indexedList.filter(e => getCell(e._2)==getCell(i) && e._2 != i).unzip._1
-    val scopeElements = (rowElements ++ colElements ++ cellElements).distinct
-    nums.diff(scopeElements)
+  val nums = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+
+  def index(row: Int, col: Int) = row * 16 + col // Gets the index in the array containing the element at the given row and column
+  def at(row: Int, col: Int): Int = elements(index(row, col)) // Gets the element at the given row and column
+  def solved: Boolean = (!elements.contains(0)) // Determines if the sudoku puzzle is filled completely (equivalent to solved since only valid moves are made)
+  def getRow(i: Int) = i / 16 // Gets the row corresponding to the specified index
+  def getCol(i: Int) = i % 16 //Gets the column corresponding to the specified index
+  def getCell(i: Int) = ((i / 16) / 4) * 4 + ((i % 16) / 4) // Gets the cell corresponding to the speecified index
+  def nextOpenSpace = elements.indexOf(0) // Gets the next empty space of the sudoku
+
+  // Comes up with a list of possible moves given an index
+  def validMoves(i: Int): List[Int] = {
+    val indexedList = elements.zipWithIndex; // Make an array of tuples of the form (element, index)
+    val rowElements = indexedList.filter(e => getRow(e._2) == getRow(i) && e._2 != i).unzip._1 // Get all other elements in the row
+    val colElements = indexedList.filter(e => getCol(e._2) == getCol(i) && e._2 != i).unzip._1 // Get all other elements in the column
+    val cellElements = indexedList.filter(e => getCell(e._2) == getCell(i) && e._2 != i).unzip._1 // Get all other elements in the cell
+    val scopeElements = (rowElements ++ colElements ++ cellElements).distinct // Combines the lists of elements
+    nums.diff(scopeElements) // Find the elements not is any row, column, or cell
   }
-  
-  def solveSpace(i:Int)={
-    if(elements(i)==0){
-      val moves=validMoves(i)
-      if(moves.size==1){
-        elements(i)=moves.head
+
+  // Solves the space at the given index is there is only one possible move there
+  def solveSpace(i: Int) = {
+    if (elements(i) == 0) {
+      val moves = validMoves(i)
+      if (moves.size == 1) {
+        elements(i) = moves.head
       }
     }
   }
-  
+
+  // Displays the sudoku is an easy to read way
   override def toString = {
     var result = ""
     for (row <- (0 to 15)) {
-      if(row%4==0){
-        result+=" -------------------------------\n"
+      if (row % 4 == 0) {
+        result += " -------------------------------\n"
       }
       for (col <- (0 to 15)) {
-        if(col%4==0){
-          result+="|"
+        if (col % 4 == 0) {
+          result += "|"
         }
         result += symbols(at(row, col))
-        if(col%4<3){
-          result+=" "
+        if (col % 4 < 3) {
+          result += " "
         }
       }
       result += "|\n"
     }
-    result+=" -------------------------------"
+    result += " -------------------------------"
     result
   }
 }
